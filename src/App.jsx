c1387f8abc2a69eef9e4607463c80a17fcda9490
AngelPASTORROJAS <angeldaniel.pastorrojas@gmail.com>
2023-05-30 11:41:26 +0200

    feat: ajout des balise de separation

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,47 @@
-import { useReducer, useState } from 'react'
-import './App.css'
-import Find from './Find'
+import { useReducer, useState } from "react";
+import "./App.css";
+import Find from "./Find";
 
 //Le state initial doit être un object
 const initialState = {
-  count:0,
-  mesage: ""
-}
+  count: 0,
+  mesage: "",
+};
 
 //gérer toutes les actions possibles sur la state
 const reducer = (state, action) => {
   switch (action.type) {
     case "incr":
-      return {...state, count: state.count+1}
+      return { ...state, count: state.count + 1 };
     case "decr":
-      return {...state, count: state.count-1}
+      return { ...state, count: state.count - 1 };
     case "number":
-      return {...state, count: action.nb}
+      return { ...state, count: action.nb };
     case "reset":
-      return {...state, count: 0}
+      return { ...state, count: 0 };
     default:
       break;
   }
-}
+};
 
 function App() {
-  const [state, dispacth] = useReducer(reducer, initialState)
+  const [state, dispacth] = useReducer(reducer, initialState);
 
   return (
     <>
-         <Find />
-    Counter : {state.count}
-    <button onClick={()=> dispacth({type:"decr"})}>-</button>
-    <button onClick={()=> dispacth({type:"incr"})}>+</button>
-    <button onClick={()=> dispacth({type:"number", nb: 30})}>Mettre à 30</button>
-    <button onClick={()=> dispacth({type:"reset"})}>Reset</button>
+      <Find />
+      <hr />
+      Counter : {state.count}
+      <button onClick={() => dispacth({ type: "decr" })}>-</button>
+      <button onClick={() => dispacth({ type: "incr" })}>+</button>
+      <button onClick={() => dispacth({ type: "number", nb: 30 })}>
+        Mettre à 30
+      </button>
+      <button onClick={() => dispacth({ type: "reset" })}>Reset</button>
+      <hr />
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
